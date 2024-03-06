ca9f29eb9b78a664a2ef3d3c597feb6f2d7ef750
nrranjith <ranjith.n1093@gmail.com>
2024-03-06 16:22:00 +0530

    updated with js

---
+++ prodct.js
@@ -0,0 +1,11 @@
+//selecting side nav bar
+let sidenav = document.getElementById("sidenav");
+let menuicon = document.getElementById("menuicon");
+let colsenav = document.getElementById("close-nav");
+
+menuicon.addEventListener("click",function() {
+  sidenav.style.right=0;
+})
+colsenav.addEventListener("click",function() {
+  sidenav.style.right ="-50%";
+})
